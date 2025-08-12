c70d0457a03c2e976b455832d21cf65d313fffbc
EduFkn <ploutzin129@gmail.com>
2025-08-12 00:39:40 -0300

    feat: add ui for sign and signup components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <h1>Hello World </h1>
       <Button>oi</Button>
     </div>
